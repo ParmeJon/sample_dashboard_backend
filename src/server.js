38112c7da0d40c2eae09b1d835c622ec1c58f63f
github username <jonathansychan@gmail.com>
2021-01-06 23:46:28 -0500

    feat: setup eslint and mongoose and mongodb

--- src/server.js
+++ src/server.js
@@ -1,6 +1,22 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
-app.listen(3000, function() {
-    console.log('listening on 3000')
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+const dbname = process.env.DBNAME;
+const port = process.env.PORT;
+
+const mongoURI = `mongodb+srv://${username}:${password}@sample-dashboard.kolop.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+  .then(() => console.log('connected to db'))
+  .catch((err) => console.log(err));
+
+app.listen(port, () => {
+  console.log('listening on 3000');
+});
